fda9965a063c1b85a0ec49a0f253b04156e25563
AONE-NLP <139965463+AONE-NLP@users.noreply.github.com>
2024-08-21 22:23:50 +0800

    Update train_arg_genia.sh

--- train_arg_genia.sh
+++ train_arg_genia.sh
@@ -1,1 +1,1 @@
-python train.py -n 5 --lr 7e-6 --num 1 --cnn_dim 400 --biaffine_size 200 --n_head 4 -b 8 -d genia  --logit_drop 0.2 --cnn_depth 1
+python train.py -n 5 --lr 7e-6 --num 1 --cnn_dim 400 --biaffine_size 200 --n_head 4 -b 8 -d genia  --logit_drop 0.2 --cnn_depth 1 --n_layer 2
