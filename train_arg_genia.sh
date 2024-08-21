b29d72f61fd1bd53a8c0d225c64944288997cb00
AONE-NLP <139965463+AONE-NLP@users.noreply.github.com>
2024-08-21 21:53:06 +0800

    Update train_arg_genia.sh

--- train_arg_genia.sh
+++ train_arg_genia.sh
@@ -1,1 +1,1 @@
-python train.py -n 15 --lr 8e-6 --num 1 --cnn_dim 200 --biaffine_size 400 --n_head 4 -b 8 -d genia  --logit_drop 0.2 --cnn_depth 3 --seed 2022
+python train.py -n 5 --lr 7e-6 --num 1 --cnn_dim 400 --biaffine_size 200 --n_head 4 -b 8 -d genia  --logit_drop 0.2 --cnn_depth 1
