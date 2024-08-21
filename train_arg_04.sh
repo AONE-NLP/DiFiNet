5fd00db8d8bf1a90286866d19b14929dee04f46f
AONE-NLP <139965463+AONE-NLP@users.noreply.github.com>
2024-08-21 18:19:39 +0800

    Update train_arg_04.sh

--- train_arg_04.sh
+++ train_arg_04.sh
@@ -1,2 +1,1 @@
-python train.py -n 80 --lr 1e-6 --num 20 --cnn_dim 120 --msize 5 --biaffine_size 120 --n_head 4 -b 32 -d ace2004 --logit_drop 0.2 --cnn_depth 1 --warmup 0.1 --seed 43
-# python train.py -n 60 --lr 1e-6 --num 25 --cnn_dim 120 --msize 5 --biaffine_size 200 --n_head 1 -b 48 -d ace2004 --logit_drop 0.1 --cnn_depth 2 --warmup 0.1 --seed 43 #roberta
+python train.py  -n 100 -d ace2004 --lr 2e-5 --cnn_dim 120 -b 16 --cnn_depth 1 --n_head 4--theta 0.35 --logit_drop 0.25  --seed 20010508
