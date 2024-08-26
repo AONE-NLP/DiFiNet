7c35cd521aa1252047383341adc14ca37b47ea80
AONE-NLP <139965463+AONE-NLP@users.noreply.github.com>
2024-08-26 20:30:02 +0800

    Update train_arg_05.sh

--- train_arg_05.sh
+++ train_arg_05.sh
@@ -1,4 +1,1 @@
-python train.py -n 100 --lr 1e-6  --cnn_dim 120 --separateness_rate 5 --msize 5 --biaffine_size 120 --n_head 4 -b 32 -d ace2005 --logit_drop 0.2 --cnn_depth 1 --num 20 --seed 43
-
-
- 
+python train.py  -n 80 -d ace2005 --lr 2e-5 --cnn_dim 120 -b 16 --cnn_depth 1 --n_head 4 --logit_drop 0.1  --seed 20010508 --n_layer 2
